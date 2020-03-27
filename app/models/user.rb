1af553796207b8e396e9858f72bfd7ab12997a5d
Pankaj Batra <pankajb@gmail.com>
2020-03-28 02:42:18 +0530

    addding devise - user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable, #:rememberable, :validatable,
+         :trackable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+  validates_presence_of :name, :email, :encrypted_password, :mobile
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  mount RailsAdmin::Engine => '/mgmt', as: 'rails_admin'
+  devise_for :users,
+             path: '',
+             path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout'
+             },
+             controllers: {
+                 sessions: 'sessions'
+             }
+  # scope module: :v1, constraints: ApiVersion.new('v1', true) do
+  #   resources :restaurants
+  # end
+
 end
