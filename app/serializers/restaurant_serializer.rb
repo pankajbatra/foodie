fabda967a0e7c77c2ab3287caa6a49db19db3e04
Pankaj Batra <pankajb@gmail.com>
2020-04-07 08:35:43 +0530

    not sending disabled meals in resturant json

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -2,7 +2,9 @@ class RestaurantSerializer < ActiveModel::Serializer
   attributes :rid, :name, :description, :open_for_delivery_now, :min_delivery_amount, :avg_delivery_time,
              :delivery_charge, :packing_charge, :tax_percent, :rating, :phone_number, :locality, :address, :latitude, :longitude
   has_many :cuisines
-  has_many :meals
+  has_many :meals do
+    object.meals.where.not(status: Meal.status.values[1])
+  end
   # belongs_to :owner, if: :is_restaurant_owner?
 
   # def is_restaurant_owner?
